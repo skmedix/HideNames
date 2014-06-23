757e502385568a81b709da9cf18ca9e38ad01f9f
thislooksfun <thislooksfun@repbot.org>
2014-06-23 16:45:50 -0500

    Removed unused imports

--- HideNames/src/com/tlf/HN/event/HNEventHandlerForge.java
+++ HideNames/src/com/tlf/HN/event/HNEventHandlerForge.java
@@ -1,19 +1,11 @@
 package com.tlf.HN.event;
 
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.entity.player.EntityPlayerMP;
-import net.minecraft.server.MinecraftServer;
-import net.minecraft.util.ChatComponentText;
 import net.minecraftforge.client.event.RenderLivingEvent;
-import net.minecraftforge.common.config.Configuration;
-import net.minecraftforge.common.config.Property;
 
 import com.tlf.HN.common.HideNames;
 
-import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
-import cpw.mods.fml.common.gameevent.PlayerEvent;
-import cpw.mods.fml.common.gameevent.TickEvent;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 

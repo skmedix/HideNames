59bf036daa1daa8a7ddb47c08bcbe964dfda440b
thislooksfun <thislooksfun@repbot.org>
2014-06-23 13:59:10 -0500

    Changed version to 1.1.0

--- HideNames/src/com/tlf/HN/common/HideNames.java
+++ HideNames/src/com/tlf/HN/common/HideNames.java
@@ -46,7 +46,7 @@ public class HideNames
 {	
 	public static final String MODID = "hidenames";
 	public static final String NAME = "Hide Names";
-	public static final String VERSION = "1.0.0";
+	public static final String VERSION = "1.1.0";
 	
 	/** The public instance */
 	@Instance(HideNames.MODID)

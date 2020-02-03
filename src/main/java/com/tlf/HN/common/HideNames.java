3a18a87dc2d47d6bb39f01fcd6ff5f2feadf358e
SKmedix <skmedixs@gmail.com>
2020-02-03 20:58:40 +0100

    :bookmark: Bumped version to 1.4.1

--- src/main/java/com/tlf/HN/common/HideNames.java
+++ src/main/java/com/tlf/HN/common/HideNames.java
@@ -20,13 +20,7 @@ import net.minecraftforge.fml.common.network.NetworkRegistry;
 import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
 import net.minecraftforge.fml.relauncher.Side;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -38,7 +32,7 @@ import java.util.logging.Logger;
 public class HideNames {
 	public static final String MODID = "hidenames";
 	public static final String NAME = "HideNames";
-	public static final String VERSION = "1.4";
+	public static final String VERSION = "1.4.1";
 
 	/**
 	 * The public INSTANCE
